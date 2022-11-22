c42353fdeef8d23b958e67a0587c31df254e9aed
Yixiang Simon Zhang <developer@simonyz.com>
2022-11-23 01:03:16 +1100

    Create initial project

---
+++ NReaderExercise/NReaderExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NReaderExercise
+//
+//  Created by Yixiang Zhang on 23/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
